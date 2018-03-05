2749dfca4f6e890b2264d1a7178697a204be32eb
souptikwgt <souptik.wgt@gmail.com>
2018-03-05 16:13:38 +0530

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('serve', ['sass'], function () {
 
     browserSync.init({
         server: "."
     });
 
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("./sass/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
+gulp.task('sass', function () {
+    return gulp.src("./sass/*.scss")
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
 
